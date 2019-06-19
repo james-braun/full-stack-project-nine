ad44963ac1444584a78a6bdcb1f9529c35c8a17d
james-braun <braun.james.robert@gmail.com>
2019-06-18 19:49:10 -0500

    final product

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// load modules.
 const express = require('express');
 const { check, validationResult } = require('express-validator/check');
 const User = require('../models').User;
@@ -9,7 +10,7 @@ const authenticateUser = require('./authenticateUser');
 // Construct a router instance.
 const router = express.Router();
 
-
+// returns an authenticated user.
 router.get('/users', authenticateUser, (req, res) => {
     const user = req.currentUser;
     res.json({
@@ -24,7 +25,10 @@ router.get('/users', authenticateUser, (req, res) => {
     });
 });
 
+// creates a user account.
 router.post('/users', [
+
+    // check for validation errors.
     check('firstName')
         .exists({ checkNull: true, checkFalsy: true })
         .withMessage('Please provide a value for "firstName"'),
@@ -39,25 +43,35 @@ router.post('/users', [
         .withMessage('Please provide a value for "password"'),
 ], (req, res) => {
 
+    // get errors.
     const errors = validationResult(req);
 
+    // if there are errors return them.
     if (!errors.isEmpty()) {
         const errorMessages = errors.array().map(error => error.msg);
 
         return res.status(400).json({ errors: errorMessages });
     }
+
+    // if the email is a valid email then create user account.
     var regExpression = /^[^@]+@[^@.]+\.[a-z]+$/i;
     if (regExpression.test(req.body.emailAddress)) {
+
+        // if the email already exists in the system return error.
         User.findAll({ where: { emailAddress: req.body.emailAddress } }).then((user) => {
             if (user.length > 0) {
                 return res.status(400).json({ errors: "User already exists." });
             } else {
                 user = req.body;
             }
+
+            // encrypt password and create user.
             user.password = bcryptjs.hashSync(user.password);
             User.create(user);
             res.status(201).redirect('/');
         });
+
+    // if the email is not a valid email return error.
     } else {
         return res.status(400).json({ errors: "Not a valid email address." });
     }
